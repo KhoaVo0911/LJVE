320f8ecae7edcfe9ead8203ed6ba7f304d3ca6dc
Hung Pham <vipkenly1@gmail.com>
2025-05-14 23:34:05 +0700

    Update project page (Still missing requirement)

--- frontend/src/hooks/routes.jsx
+++ frontend/src/hooks/routes.jsx
@@ -5,6 +5,11 @@ const routes = {
   journalDetail: "/journals/:title",
   about: "/about",
   projects: "/projects",
+  documentary: "/documentary",
+  musicVideo: "/musicVideo",
+  original: "/original",
+  shortFilm: "/shortFilm",
+  commercial: "/commercial",
 };
 
 export default routes;
