aaaa6ab7e5355865b7bf55a06d3189cedc912a5d
Hung Pham <vipkenly1@gmail.com>
2025-05-14 22:16:40 +0700

    Update navigation page and UI in Journal

--- frontend/src/components/layout/body/index.jsx
+++ frontend/src/components/layout/body/index.jsx
@@ -1,12 +1,18 @@
 import { AboutPage } from "@/pages/about";
 import { HomePage } from "@/pages/home";
+import { JournalPage } from "@/pages/journal";
+import { ProjectsPage } from "@/pages/projects";
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 export const AppBody = () => {
   return (
-    <div>
-      <AboutPage></AboutPage>
-      <HomePage></HomePage>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/journals" element={<JournalPage />} />
+      <Route path="/projects" element={<ProjectsPage />} />
+    </Routes>
   );
 };
--- frontend/src/components/layout/header/navigation/index.jsx
+++ frontend/src/components/layout/header/navigation/index.jsx
@@ -5,33 +5,41 @@ import {
   NavigationMenuLink,
 } from "@/components/ui/navigation-menu";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import routes from "@/hooks/routes";
 
 export const NavigationBar = () => {
-  const navBarCustom = "font-[Gilroy-Light] text-white text-[24px] px-4";
+  const navBarBaseClass =
+    "font-[Gilroy-Light] text-[24px] text-white px-4 transition-all ";
+
   const items = [
-    { label: "Journal", to: "/" + routes.journals },
-    { label: "About", to: "/" + routes.about },
+    { label: "Journal", to: routes.journals },
+    { label: "About", to: routes.about },
     { label: "Home", to: routes.home },
-    { label: "Projects", to: "/" + routes.projects },
+    { label: "Projects", to: routes.projects },
   ];
 
   return (
     <NavigationMenu>
-      <NavigationMenuList className="flex items-center ">
+      <NavigationMenuList className="flex items-center">
         {items.map((item, index) => (
           <React.Fragment key={item.label}>
             <NavigationMenuItem>
-              <Link to={item.to} legacyBehavior passHref>
-                <NavigationMenuLink className={navBarCustom}>
-                  {item.label}
-                </NavigationMenuLink>
-              </Link>
+              <NavLink to={item.to}>
+                {({ isActive }) => (
+                  <NavigationMenuLink
+                    className={`${navBarBaseClass} ${
+                      isActive ? "font-extrabold" : "font-normal"
+                    }`}
+                  >
+                    {item.label}
+                  </NavigationMenuLink>
+                )}
+              </NavLink>
             </NavigationMenuItem>
             {index < items.length - 1 && (
               <div
-                className="h-[1px] w-[48px] !bg-white mx-1 "
+                className="h-[1px] w-[48px] !bg-white mx-1"
                 aria-hidden="true"
               />
             )}
