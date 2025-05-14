12737da8accf88353ddd54a86a6bbc9ef7808b4f
Võ Đăng Khoa <133868589+KhoaVo0911@users.noreply.github.com>
2025-05-14 21:48:08 +0700

    routes

--- frontend/src/components/layout/body/index.jsx
+++ frontend/src/components/layout/body/index.jsx
@@ -1,15 +1,12 @@
 import { AboutPage } from "@/pages/about";
-import { JournalPage } from "@/pages/journal";
 import { HomePage } from "@/pages/home";
 import React from "react";
-import { Outlet } from "react-router-dom";
 
 export const AppBody = () => {
   return (
-    <>
-      {/* <AboutPage></AboutPage> */}
-      {/* <JournalPage></JournalPage> */}
+    <div>
+      <AboutPage></AboutPage>
       <HomePage></HomePage>
-    </>
+    </div>
   );
 };
