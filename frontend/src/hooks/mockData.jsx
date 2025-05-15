679ef3bc906093f8af9e4a061f08bf16a51fc09e
Hung Pham <vipkenly1@gmail.com>
2025-05-15 23:09:24 +0700

    Update About me page

--- frontend/src/hooks/mockData.jsx
+++ frontend/src/hooks/mockData.jsx
@@ -1,6 +1,10 @@
-export const images = [
-  "assets/459424739_1048003410336309_4209133926252165288_n.png",
-  "assets/IMG_3556.jpg",
+export const imagesAboutMe = [
+  "assets/film/about/1.jpg",
+  "assets/film/about/2.jpg",
+  "assets/film/about/3.jpg",
+  "assets/film/about/4.jpg",
+  "assets/film/about/5.jpg",
+  "assets/film/about/6.jpg",
 ];
 
 export const journalData = [
