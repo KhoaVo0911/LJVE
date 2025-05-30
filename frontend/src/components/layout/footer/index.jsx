0c9080b15a2c933c52707fd7f38291bef96d9ce3
Hung Pham <vipkenly1@gmail.com>
2025-05-30 23:28:49 +0700

    Update code gg

--- frontend/src/components/layout/footer/index.jsx
+++ frontend/src/components/layout/footer/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { InformationIcon } from "../information";
+import ActiveUsersDisplay from "./activeUsersDisplay";
 
 export const AppFooter = () => {
   return (
     <div className="mt-[40px] flex flex-row justify-center gap-4 mb-[25px]">
       <InformationIcon direction="row" gap="gap-4" />
+      <ActiveUsersDisplay />
     </div>
   );
 };
